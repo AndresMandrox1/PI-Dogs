bfe70a90d047d03ea35c1bc7ef901b9d42b60fef
AndresMandrox1 <andreslopez778@rocketmail.com>
2022-05-24 03:39:08 -0600

    make it better

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,9 +1,38 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST , DB_NAME} = process.env;
 
-var db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-  logging: false,
-});
+let db =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+        { logging: false, native: false }
+      );
+
+// var db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+//   logging: false,
+// });
 
 const Breed = db.define(
   "breed",
